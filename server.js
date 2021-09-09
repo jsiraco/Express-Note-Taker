ef70d636a266e4a869e6965cd30015bf794f4842
Jas <ragnarock.josef@gmail.com>
2021-09-08 20:29:24 -0400

    data can now be written to db.json from server

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const path = require("path");
-const db = require("./package.json");
+const fs = require("fs");
+
+const noteData = require("./db/db.json");
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+  const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
 
 
 const app = express();
@@ -8,7 +27,6 @@ const PORT = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
 
 app.use(express.static("public"));
 
@@ -16,17 +34,35 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "/public/index.html"))
 });
 
-app.get("/api/notes", (req, res) => {
-    res.sendfile(path.join(_dirname, "/public/notes.html"))
+// app.get("/notes", (req, res) => {
+//     res.sendfile(path.join(_dirname, "/public/notes.html"));
+// });
+
+app.get("/api/db", (req, res) => { 
+    res.json(noteData);
+    console.info(`Request recieved`);
 });
 
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
-
-app.post("/api/notes", (req, res) => {
+app.post("/api/db", (req, res) => {
     console.info(`${req.method} request was received`)
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        res.json("Note added successfully");
+    } else {
+        res.error("Error adding note");
+    }
 });
 
 app.listen(PORT, () =>
