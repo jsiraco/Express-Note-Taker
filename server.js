b755d9ac8982befe502901536b9040eb9e512f33
Jas <ragnarock.josef@gmail.com>
2021-09-12 15:38:59 -0400

    added sample data & changed paths

--- server.js
+++ server.js
@@ -1,65 +1,72 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 
 const noteData = require("./db/db.json");
 
-const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
+const readFromFile = util.promisify(fs.readFile);
 
-  const writeToFile = (destination, content) =>
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
+const PORT = process.env.port || 3001;
 
 const app = express();
-const PORT = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/index.html"))
+// });
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
-});
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/public/notes.html"))
+// });
 
-app.get("/api/db", (req, res) => { 
-    res.json(noteData);
-    console.info(`Request recieved`);
-});
+app.get("/", (req, res) => res.render("/public/index"));
 
-app.post("/api/db", (req, res) => {
-    console.info(`${req.method} request was received`)
+app.get("/notes", (req, res) => res.render("/public/notes"));
 
-    const { title, text } = req.body;
+// app.get("/api/db", (req, res) => {
+//   res.json(noteData);
+//   console.info(`Request recieved`);
+// });
 
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-        };
+// app.post("/api/db", (req, res) => {
+//   console.info(`${req.method} request was received`)
 
-        readAndAppend(newNote, "./db/db.json");
-        res.json("Note added successfully");
-    } else {
-        res.error("Error adding note");
-    }
-});
+//   const { title, text } = req.body;
+
+//   if (req.body) {
+//     const newNote = {
+//       title,
+//       text,
+//     };
+
+//     readAndAppend(newNote, "./db/db.json");
+//     res.json("Note added successfully");
+//   } else {
+//     res.error("Error adding note");
+//   }
+// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
